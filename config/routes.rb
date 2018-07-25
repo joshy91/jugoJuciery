e8638d05e8f69ebe4b3408ea5c9ff3464caf9563
joshy91 <joshua.davidsonsga@gmail.com>
2018-07-25 10:19:10 -0500

    Adjusted Navbar, set structure for locations page, adjusted pages routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   get 'home' => 'pages#home'
-  get 'pages/team'
-  get 'pages/locationsAndhours'
-  get 'pages/franchiseInfo'
-  get 'pages/careerInfo'
-  get 'pages/home'
-  get 'pages/team'
-  get 'pages/locations'
+  get 'team' => 'pages#team'
+  get 'locationsNhours' => 'pages#locationsAndhours'
+  get 'franchiseInfo' => 'pages#franchiseInfo'
+  get 'careerInfo' => 'pages#careerInfo'
   root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
